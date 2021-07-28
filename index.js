c1f85d634e1adf08cd194dbe52f5915d2065b23d
Julia <51319929+JuliaKyung@users.noreply.github.com>
2021-07-27 21:51:00 -0700

    Added interaction
    added jquery interaction on work previews

---
+++ index.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $(".prevImg1").mouseover(function(){
+        $(".prevImg1").css("opacity", "0.4");
+    });
+    $(".prevImg1").mouseout(function(){
+        $(".prevImg1").css("opacity", "1");
+    });
+});
+$(document).ready(function(){
+    $(".prevImg2").mouseover(function(){
+        $(".prevImg2").css("opacity", "0.4");
+    });
+    $(".prevImg2").mouseout(function(){
+        $(".prevImg2").css("opacity", "1");
+    });
+});
+$(document).ready(function(){
+    $(".prevImg3").mouseover(function(){
+        $(".prevImg3").css("opacity", "0.4");
+    });
+    $(".prevImg3").mouseout(function(){
+        $(".prevImg3").css("opacity", "1");
+    });
+});
